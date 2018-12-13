a6e09903a115d34b3c45f1c3fd5d3f5fd62cf43e
Dustin Schau <dustinschau@gmail.com>
2018-12-13 10:25:50 -0600

    chore: add seo

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,6 +8,7 @@ function BlogPost({ data }) {
   const { post } = data
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} description={post.frontmatter.description || post.excerpt} />
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </Layout>
@@ -20,7 +21,9 @@ export const blogPostBySlugQuery = graphql`
   query GetBlogPostBySlug($slug: String!) {
     post: markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      excerpt(pruneLength: 150)
       frontmatter {
+        description
         title
       }
     }
